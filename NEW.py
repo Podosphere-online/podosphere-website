499d7f5912bd9d3017738868a88e9d695a63458e
AyanAhmedKhan <145579926+AyanAhmedKhan@users.noreply.github.com>
2024-08-27 15:41:22 +0530

    Add files via upload

---
+++ NEW.py
@@ -0,0 +1,22 @@
+# c1=complex(2,5)
+# print(c1)
+# c2=complex(7,8)
+# print(c2)
+# print(c1+c2).
+
+class A:
+    def __init__(Self):
+        print("ConstructorA")
+class B(A):    
+    def __init__(Self):
+        print("ConstructorB")
+
+    b1= B()
+    
+print (b1) 
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+c1=complex(2,5)
+# print(c1)
+# c2=complex(7,8)
+# print(c2)
+# print(c1+c2)
